1ea3eca2bc495f4ba42e1b3f3d9ea4cb5a9c318b
Jason Paquette <jason_paquette56@hotmail.com>
2018-02-20 17:04:52 -0500

    Perfectionnement de la fonction de peuplement

--- mes_modules/peupler/index.js
+++ mes_modules/peupler/index.js
@@ -1,16 +1,29 @@
 "use strict";
 const tableau = require('./tableaux.js');
-const max = tableau.length;
+const tabNom = tableau.tabNom;
+const tabPrenom = tableau.tabPrenom;
+const tabCourriel = tableau.tabCourriel;
+const tabTelephone = tableau.tabTelephone;
+const max = tabNom.length;
 console.log('max = ' + max);
 const peupler_json = () => {
-	let position;
+	let tabToutesLesPersonnes = [];
 	let tabPersonne = [];
+	let position = 0;
 	for (let k=0;k<20;k++) {
-		let position = Math.floor(Math.random()*max);
-		tabPersonne.push(tableau[position]);
+		tabPersonne = [];
+		position = Math.floor(Math.random()*tabNom.length);
+		tabPersonne.push(tabNom[position]);
+		position = Math.floor(Math.random()*tabPrenom.length);
+		tabPersonne.push(tabPrenom[position]);
+		position = Math.floor(Math.random()*tabCourriel.length);
+		tabPersonne.push(tabCourriel[position]);
+		position = Math.floor(Math.random()*tabTelephone.length);
+		tabPersonne.push(tabTelephone[position]);
+		tabToutesLesPersonnes.push(tabPersonne);
 	}
-	console.log("tableau personne : " + tabPersonne);
-	return(tabPersonne);
+	console.log("tableau personne : " + tabToutesLesPersonnes);
+	return(tabToutesLesPersonnes);
 }
 
 module.exports = peupler_json;
--- mes_modules/peupler/tableaux.js
+++ mes_modules/peupler/tableaux.js
@@ -1,8 +1,35 @@
 "use strict";
-let tableau = [
-	["Paquette", "Jason", "afan@hotmail.com", "514-555-5555"],
-	["Duthoy", "Laurent", "l.duthoy@hotmail.fr", "514-645-9869"],
-	["Payeur", "Valérie", "valeriePayeur@yahoo.fr", "514-783-1328"]
+// let tableau = [
+// 	["Paquette", "Jason", "afan@hotmail.com", "514-555-5555"],
+// 	["Duthoy", "Laurent", "l.duthoy@hotmail.fr", "514-645-9869"],
+// 	["Payeur", "Valérie", "valeriePayeur@yahoo.fr", "514-783-1328"]
+// ]
+
+let tabNom = [
+	"Paquette",
+	"Duthoy",
+	"Payeur"
 ]
 
-module.exports = tableau;
+let tabPrenom = [
+	"Jason",
+	"Laurent",
+	"Valérie"
+]
+
+let tabCourriel = [
+	"afan@hotmail.com",
+	"l.duthoy@hotmail.fr",
+	"valeriePayeur@yahoo.fr"
+]
+
+let tabTelephone = [
+	"514-555-5555",
+	"514-645-9869",
+	"514-783-1328"
+]
+
+module.exports.tabNom = tabNom;
+module.exports.tabPrenom = tabPrenom;
+module.exports.tabCourriel = tabCourriel;
+module.exports.tabTelephone = tabTelephone;
